109bc48bff2088c6d06ef9f9640a79f6a54edf46
Iftikhar-Shams-Niloy <81116328+Iftikhar-Shams-Niloy@users.noreply.github.com>
2021-04-01 18:51:01 +0600

    Add files via upload

---
+++ Drawing Shapes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("    (*)    \n");
+    printf("   (   )   \n");
+    printf("  (     )  \n");
+    printf(" (  O O  ) \n");
+    printf(" (__---__) \n" );
+    printf("  ||   ||  \n");
+    printf("  //   //  \n");
+    printf("^^^^^^^^^^^^^\n");
+
+    printf("      /|    \n");
+    printf("     /  |   \n");
+    printf("    /_  _|  \n");
+    printf("   /(*)(*)| \n");
+    printf("  /  (0)   | \n");
+    printf(" /__________|\n");
+    printf("    Y   Y    \n");
+    printf("^^^^^^^^^^^^^^^^\n");
+
+    printf("  _########## \n");
+    printf("  |         | \n");
+    printf("  |  >   <  | \n");
+    printf("  |    ^    | \n");
+    printf("  | =)    =)| \n");
+    printf("  |         | \n");
+    printf("  |_________| \n");
+    printf("   |_|   |_|  \n");
+    printf("^^^^^^^^^^^^^^^^^");
+    return 0;
+}
